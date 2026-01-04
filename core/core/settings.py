99f0e6802ec45da87c774a1058ba23fa06af2c18
narevent <mr.rhythm@live.nl>
2026-01-04 11:53:58 +0100

    move scripts

--- core/core/settings.py
+++ core/core/settings.py
@@ -6,7 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS=['vumgames.com','www.vumgames.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
